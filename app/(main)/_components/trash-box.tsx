33d45210f3b4ade709b2c0c9eae1f1538a3e0369
AhmadHRai <ahmadhalraie@gmail.com>
2024-12-25 15:16:53 +0300

    feat: finish trash box

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import { ConfirmModal } from "@/components/modals/confirm-modal";
 import { Spinner } from "@/components/spinner";
 import { Input } from "@/components/ui/input";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useMutation, useQuery } from "convex/react";
-import { Search } from "lucide-react";
+import { Search, Trash, Undo } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { toast } from "sonner";
@@ -28,7 +29,7 @@ export default function TrashBox() {
   };
 
   const onRestore = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
     documentId: Id<"documents">
   ) => {
     event.stopPropagation();
@@ -83,10 +84,25 @@ export default function TrashBox() {
             role="button"
             key={document._id}
             className="flex items-center justify-between text-primary text-sm rounded-sm w-full hover:bg-primary/5 "
-            onClick={() => onClick(document._id)}>
+            onClick={() => onClick(document._id)}
+          >
             <span className="truncate pl-2">{document.title}</span>
             <div className="flex items-center">
-              <div></div>
+              <div
+                role="button"
+                className="rounded-sm p-2 hover:bg-neutral-200"
+                onClick={(e) => onRestore(e, document._id)}
+              >
+                <Undo className="h-4 w-4 text-muted-foreground" />
+              </div>
+              <ConfirmModal onConfirm={() => onRemove(document._id)}>
+                <div
+                  role="button"
+                  className="rounded-sm p-2 hover:bg-neutral-200"
+                >
+                  <Trash className="h-4 w-4 text-muted-foreground" />
+                </div>
+              </ConfirmModal>
             </div>
           </div>
         ))}
