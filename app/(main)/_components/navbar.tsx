803b14e2725cd30a7bd0edb77baddbf32e95e8d9
AhmadHRai <ahmadhalraie@gmail.com>
2024-12-29 10:33:48 +0300

    feat: add menu and skeleton

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import Banner from "./banner";
+import Menu from "./menu";
 import Title from "./title";
 
 interface NavbarProps {
@@ -22,8 +23,11 @@ export default function Navbar({ isCollapsed, onResetWidth }: NavbarProps) {
 
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-4">
+      <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
+        <div className="flex items-center gap-x-2">
+          <Menu.Skeleton />
+        </div>
       </nav>
     );
   }
@@ -44,6 +48,9 @@ export default function Navbar({ isCollapsed, onResetWidth }: NavbarProps) {
         )}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
       {document.isArchived && <Banner documentId={document._id} />}
