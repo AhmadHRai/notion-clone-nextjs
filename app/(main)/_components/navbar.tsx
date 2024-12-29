3aa1b51d4ead8f2cd14ad20e0445c1a25d8b1e77
AhmadHRai <ahmadhalraie@gmail.com>
2024-12-29 09:47:37 +0300

    feat: add title

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -5,6 +5,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
+import Title from "./title";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -19,7 +20,11 @@ export default function Navbar({ isCollapsed, onResetWidth }: NavbarProps) {
   });
 
   if (document === undefined) {
-    return <p>Loading...</p>; // Todo: skeleton
+    return (
+      <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-4">
+        <Title.Skeleton />
+      </nav>
+    );
   }
 
   if (document === null) {
@@ -36,7 +41,9 @@ export default function Navbar({ isCollapsed, onResetWidth }: NavbarProps) {
             className="h-6 w-6 text-muted-foreground"
           />
         )}
-        <div className="flex items-center justify-between w-full">Navbar</div>
+        <div className="flex items-center justify-between w-full">
+          <Title initialData={document} />
+        </div>
       </nav>
     </>
   );
